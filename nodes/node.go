0a3ba683b2a93389e889b3754f78e752681955fa
Egor Dubin <delisa.sama@gmail.com>
2022-05-27 14:38:31 +0300

    Clean code

--- nodes/node.go
+++ nodes/node.go
@@ -4,11 +4,6 @@ type Node interface {
 	Accept(visitor TranslateVisitor) string
 }
 
-type NodeWithRight interface {
-	Node
-	Right() Node
-}
-
 type NodeWithChilds interface {
 	Node
 	Childs() []Node
@@ -17,4 +12,26 @@ type NodeWithChilds interface {
 type NodeWithName interface {
 	Node
 	Name() NameNode
+}
+
+type TranslateVisitor interface {
+	TranslateAndNode(node AndNode) string
+	TranslateArrayNode(node ArrayNode) string
+	TranslateEqNode(node EqNode) string
+	TranslateGeNode(node GeNode) string
+	TranslateGtNode(node GtNode) string
+	TranslateInNode(node InNode) string
+	TranslateLeNode(node LeNode) string
+	TranslateLtNode(node LtNode) string
+	TranslateNameNode(node NameNode) string
+	TranslateNeNode(node NeNode) string
+	TranslateNotNode(node NotNode) string
+	TranslateNullNode(node NullNode) string
+	TranslateOrNode(node OrNode) string
+	TranslateStringNode(node StringNode) string
+	TranslateValueNode(node ValueNode) string
+	TranslateTimeNode(n TimeNode) string
+	TranslateIntNode(n IntNode) string
+	TranslateFloatNode(n FloatNode) string
+	TranslateUintNode(n UintNode) string
 }
