e43e35428ac127744d5847fe855b0c6388a2848a
Egor Dubin <delisa.sama@gmail.com>
2022-06-18 16:01:19 +0300

    Add sort

--- statement/statement.go
+++ statement/statement.go
@@ -2,11 +2,13 @@ package statement
 
 import (
 	"github.com/Delisa-sama/stmt-builder/nodes"
+	"github.com/Delisa-sama/stmt-builder/sort"
 )
 
 // Statement represents tree of nodes that can be translated to query
 type Statement struct {
 	root nodes.Node
+	sort sort.Sort
 }
 
 // Operator represents operator for construct statement
@@ -18,6 +20,7 @@ type Operator interface {
 func New(leftOperand string, op Operator) Statement {
 	return Statement{
 		root: op.Node(leftOperand),
+		sort: nil,
 	}
 }
 
@@ -28,9 +31,14 @@ func Empty() Statement {
 	}
 }
 
-// Root returns root node
-func (s Statement) Root() nodes.Node {
+// GetRoot returns root node
+func (s Statement) GetRoot() nodes.Node {
 	return s.root
+}
+
+// GetSort returns sort
+func (s Statement) GetSort() sort.Sort {
+	return s.sort
 }
 
 // IsEmpty returns true if statement is empty
@@ -41,26 +49,35 @@ func (s Statement) IsEmpty() bool {
 // Not returns new statement with negotiate
 func Not(statement Statement) Statement {
 	return Statement{
-		root: nodes.NewNotNode(statement.Root()),
+		root: nodes.NewNotNode(statement.GetRoot()),
 	}
 }
 
 // And returns a new statement concatenating the two with AND operator
 func (s Statement) And(another Statement) Statement {
 	if s.IsEmpty() {
-		return Statement{root: another.Root()}
+		return Statement{root: another.GetRoot()}
 	}
 	return Statement{
-		root: nodes.NewAndNode(s.Root(), another.Root()),
+		root: nodes.NewAndNode(s.GetRoot(), another.GetRoot()),
 	}
 }
 
 // Or returns a new statement concatenating the two with OR operator
 func (s Statement) Or(another Statement) Statement {
 	if s.IsEmpty() {
-		return Statement{root: another.Root()}
+		return Statement{root: another.GetRoot()}
 	}
 	return Statement{
-		root: nodes.NewOrNode(s.Root(), another.Root()),
+		root: nodes.NewOrNode(s.GetRoot(), another.GetRoot()),
 	}
+}
+
+// Sort returns statement with sort
+func (s Statement) Sort(columnNames []string, direction sort.Direction) Statement {
+	if len(columnNames) == 0 {
+		return s
+	}
+	s.sort = sort.NewSort(columnNames, direction)
+	return s
 }
